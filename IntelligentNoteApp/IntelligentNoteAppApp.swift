8b0095089686b0f2be5a29739850287b2100c004
gasparnd <gaspardolcemascolo@gmail.com>
2026-01-02 19:02:57 -0300

    Initial Commit

---
+++ IntelligentNoteApp/IntelligentNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntelligentNoteAppApp.swift
+//  IntelligentNoteApp
+//
+//  Created by Gaspar Dolcemascolo on 02-01-26.
+//
+
+import SwiftUI
+
+@main
+struct IntelligentNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
