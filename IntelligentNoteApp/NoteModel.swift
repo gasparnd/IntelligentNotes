fecc82e22320447945c4e7035104962708e1cc25
gasparnd <gaspardolcemascolo@gmail.com>
2026-01-04 08:26:37 -0300

    Add Note Model for SwiftData

--- IntelligentNoteApp/NoteModel.swift
+++ IntelligentNoteApp/NoteModel.swift
@@ -6,9 +6,43 @@
 //
 
 import Foundation
+import SwiftData
 
 struct Note: Identifiable, Hashable {
-    let id = UUID()
+    var id = UUID()
     var title: String
     var body: String
+    var createdAt = Date()
+}
+
+
+// MARK: - SwiftData Model
+
+@Model
+final class NoteModel {
+    var id: UUID
+    var title: String
+    var body: String
+    var createdAt: Date
+    
+    init(
+        id: UUID,
+        title: String,
+        body: String,
+        createdAt: Date
+    ) {
+        self.id = UUID()
+        self.title = title
+        self.body = body
+        self.createdAt = createdAt
+    }
+    
+    func toNote() -> Note {
+        Note(
+            id: self.id,
+            title: self.title,
+            body: self.body,
+            createdAt: self.createdAt
+        )
+    }
 }
