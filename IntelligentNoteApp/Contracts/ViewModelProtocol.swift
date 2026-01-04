6fcf0dd01034dfb23b6a67152475c55e10362343
gasparnd <gaspardolcemascolo@gmail.com>
2026-01-04 08:50:41 -0300

    Add ViewModelProtocol

---
+++ IntelligentNoteApp/Contracts/ViewModelProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  ViewModelProtocol.swift
+//  IntelligentNoteApp
+//
+//  Created by Gaspar Dolcemascolo on 04-01-26.
+//
+
+import Foundation
+
+protocol ViewModelProtocol: ObservableObject {
+    /// Get Notes
+    func getNotes() -> [Note]
+    /// Create Note
+    func createNote(from: Note)
+    /// Update Note
+    func updateNote(id: UUID, with: Note)
+    /// Delete Note
+    func deleteNote(id: UUID)
+    /// Donwload Note
+    func downloadNote(id: UUID) -> Bool
+}
